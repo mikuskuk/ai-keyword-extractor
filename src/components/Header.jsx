7eebef45ae18bd2649b7bed303cb95c1c009a0fa
Mikus Kukainis <mikuskukainis@gmail.com>
2023-09-22 17:14:33 +0300

    Added header and footer components

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Heading, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.svg'
+
+const Header = () => {
+  return (
+    <>
+        <Image src={logo} alt='logo' width={ 100 } marginBottom='1rem' />
+        <Heading color='white' marginBottom='1rem'>
+            AI Keyword Extractor
+        </Heading>
+        <Text fontSize={25} textAlign='center'>
+            Paste in your text to get keywords from it
+        </Text>
+    </>
+  )
+}
+
+export default Header
