805a1161119bc9b0a90aa25ae8dea6e53df28df8
Mikus Kukainis <mikuskukainis@gmail.com>
2023-09-22 17:52:17 +0300

    Added API to OpenAI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Heading, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.png'
 
 const Header = () => {
   return (
     <>
         <Image src={logo} alt='logo' width={ 100 } marginBottom='1rem' />
         <Heading color='white' marginBottom='1rem'>
-            AI Keyword Extractor
+            AI Keyword Extractor for ETSY Listings
         </Heading>
         <Text fontSize={25} textAlign='center'>
-            Paste in your text to get keywords from it
+            Paste in your product description to get keywords for your listing
         </Text>
     </>
   )
